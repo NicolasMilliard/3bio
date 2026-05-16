2ddc8887d635c0c67a9eca637a6b1c8f6c2fe8eb
Nicolas Milliard <73180392+NicolasMilliard@users.noreply.github.com>
2026-05-16 18:38:04 +0200

    feat: Add IdentitySection component and integrate it into EditorScreen

---
+++ src/features/editor/components/IdentitySection.tsx
@@ -0,0 +1,30 @@
+import type { InBioProfile } from '@/schemas/inBioMetadata.schema';
+import { useFormContext, useWatch } from 'react-hook-form';
+import type { MetadataFormValues } from '../schemas/metadataForm.schema';
+
+import { Identity } from '@/features/profile/components';
+
+export const IdentitySection = ({
+  lensHandle,
+  profile,
+}: {
+  lensHandle: string;
+  profile: InBioProfile;
+}) => {
+  const { control } = useFormContext<MetadataFormValues>();
+  const formAvatar = useWatch({ control, name: 'avatar.preview' }) ?? undefined;
+  const formName = useWatch({ control, name: 'name' });
+  const formBio = useWatch({ control, name: 'bio' });
+
+  const formProfile = {
+    avatar: formAvatar,
+    name: formName,
+    bio: formBio,
+  };
+
+  const mergedProfile = {
+    ...profile,
+    ...formProfile,
+  };
+  return <Identity lensHandle={lensHandle} profile={mergedProfile} />;
+};
--- src/features/editor/components/index.ts
+++ src/features/editor/components/index.ts
@@ -1,6 +1,7 @@
 export * from './BrandingSection';
 export * from './CoverPictureSection';
 export * from './EditorForm';
+export * from './IdentitySection';
 export * from './SidebarEditor';
 export * from './StatisticsSection';
 export * from './StatisticsToggle';
