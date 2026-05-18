5d1decda63950f06071f8416256780fc523962de
Nicolas Milliard <73180392+NicolasMilliard@users.noreply.github.com>
2026-05-18 18:51:09 +0200

    refactor: Simplify SubscriptionCard component structure

--- src/features/homepage/components/SubscriptionCard.tsx
+++ src/features/homepage/components/SubscriptionCard.tsx
@@ -15,43 +15,41 @@ export const SubscriptionCard = ({
   variant,
   price,
   features,
-}: SubscriptionPlan) => {
-  return (
-    <Card className="min-h-85 max-w-88 flex-1/2 py-0 shadow-none ring-0">
-      <CardHeader
-        className={`${variant === 'free' ? 'bg-muted' : 'bg-accent'} py-4`}
-      >
-        <CardTitle>
-          <Text variant="h2" className="capitalize">
-            {variant}
-          </Text>
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <div className="mb-8">
-          <div className="flex items-center gap-6">
-            <Text variant="h2">{price.value}</Text>
-            {price.previousValue && (
-              <Text className="text-muted-foreground line-through">
-                {price.previousValue}
-              </Text>
-            )}
-            {price.additionalContent && <>{price.additionalContent}</>}
-          </div>
-          <Text className="text-muted-foreground">{price.subtitle}</Text>
+}: SubscriptionPlan) => (
+  <Card className="min-h-85 max-w-88 flex-1/2 py-0 shadow-none ring-0">
+    <CardHeader
+      className={`${variant === 'free' ? 'bg-muted' : 'bg-accent'} py-4`}
+    >
+      <CardTitle>
+        <Text variant="h2" className="capitalize">
+          {variant}
+        </Text>
+      </CardTitle>
+    </CardHeader>
+    <CardContent>
+      <div className="mb-8">
+        <div className="flex items-center gap-6">
+          <Text variant="h2">{price.value}</Text>
+          {price.previousValue && (
+            <Text className="text-muted-foreground line-through">
+              {price.previousValue}
+            </Text>
+          )}
+          {price.additionalContent && <>{price.additionalContent}</>}
         </div>
-        <Text className="font-bold">{features.title}</Text>
-        <ul>
-          {features.items.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </CardContent>
-      <CardFooter className="mx-auto pb-6">
-        <Button asChild variant={variant === 'free' ? 'outline' : 'default'}>
-          <Link to="/dashboard">Get started</Link>
-        </Button>
-      </CardFooter>
-    </Card>
-  );
-};
+        <Text className="text-muted-foreground">{price.subtitle}</Text>
+      </div>
+      <Text className="font-bold">{features.title}</Text>
+      <ul>
+        {features.items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </CardContent>
+    <CardFooter className="mx-auto pb-6">
+      <Button asChild variant={variant === 'free' ? 'outline' : 'default'}>
+        <Link to="/dashboard">Get started</Link>
+      </Button>
+    </CardFooter>
+  </Card>
+);
