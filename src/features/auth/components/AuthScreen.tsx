9d5b3888ef1226010b3a66f070df0d4c30a1e422
Nicolas Milliard <73180392+NicolasMilliard@users.noreply.github.com>
2026-07-30 09:44:56 +0200

    fix: vertical spacing

--- src/features/auth/components/AuthScreen.tsx
+++ src/features/auth/components/AuthScreen.tsx
@@ -3,7 +3,7 @@ import { AuthButton } from '@/features/auth/components';
 
 export const AuthScreen = () => {
   return (
-    <section className="mt-30 flex flex-col items-center justify-center gap-6 px-4 text-center">
+    <section className="my-30 flex flex-col items-center justify-center gap-6 px-4 text-center">
       <Text variant="h1">Connect your wallet</Text>
       <Text className="max-w-97">
         Access your 3bio dashboard by connecting your wallet. Your identity and
